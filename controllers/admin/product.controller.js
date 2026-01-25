813a9702981b0e69a5389f264142ca6a85bc2a52
PhanVuLinh <linhphanzzz04@gmail.com>
2026-01-25 15:50:28 +0700

    1.3 Làm giao diện trang danh sách sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+//call data
+const Product = require ("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  });
   res.render("admin/pages/products/index", {
-    titlePage: "Trang sản phẩm"
+    titlePage: "Trang sản phẩm",
+    products: products
   });
 };
