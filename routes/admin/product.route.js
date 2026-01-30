a7b3e124daf308897e848d7c06ae7177a2f3efe2
PhanVuLinh <linhphanzzz04@gmail.com>
2026-01-30 23:47:53 +0700

    Tính năng tạo mới sản phẩm (Uploads Ảnh , Uploads 1 ảnh)

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const multer = require('multer')
 const router = express.Router();
+const storageMulter = require("../../helpers/storageMulter");
+
+const upload = multer({storage: storageMulter()});
 
 const controller = require("../../controllers/admin/product.controller");
 
@@ -9,10 +13,14 @@ router.patch('/change-status/:status/:id', controller.changeStatus); ///:status 
 
 router.patch('/change-multi', controller.changeMulti);
 
-router.delete('/delete/:id', controller.deleteItem); 
+router.delete('/delete/:id', controller.deleteItem);
 
 router.get('/create', controller.create); //Trả về giao diện
 
-router.post('/create', controller.createPost);
+router.post(
+  '/create',
+  upload.single('thumbnail'),
+  controller.createPost
+);
 
-module.exports = router;
+module.exports = router; 
