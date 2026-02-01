11ca6f3b9e1d9d57b33df6df651136969cab1a9a
PhanVuLinh <linhphanzzz04@gmail.com>
2026-02-01 14:37:36 +0700

    Làm trang chi tiết sản phẩm Bên (CLIENT)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+// [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
@@ -9,7 +10,29 @@ module.exports.index = async (req, res) => {
     return item;
   });
   res.render("client/pages/products/index", {
-    titlePage: "Sản phẩm",
+    titlePage: "Danh sách sản phẩm",
     products: newProducts
   });
+};
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const slug = req.params.slug;
+    const find = {
+      deleted: false,
+      status: "active",
+      slug: slug
+    };
+    const product = await Product.findOne(find);
+
+    console.log(product);
+
+    res.render("client/pages/products/detail", {
+      titlePage: product.title,
+      product: product
+    });
+  } catch (error) {
+    res.redirect(`/products`);
+  }
 };
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,4 +5,6 @@ const controller = require("../../controllers/client/product.controller");
 
 router.get('/', controller.index);
 
+router.get('/:slug', controller.detail); //chi tieets sanr pham
+
 module.exports = router;
