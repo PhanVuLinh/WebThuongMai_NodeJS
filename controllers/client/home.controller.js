90d781b88ced0de5b5a440ad185bc8dc20a68406
PhanVuLinh <linhphanzzz04@gmail.com>
2026-02-22 14:26:20 +0700

    4.1 Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,19 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 //[GET] /
 module.exports.index = async (req, res) => {
+  //Lấy sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    status: "active",
+    deleted: false
+  });
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  //end 
+  
   res.render("client/pages/home/index", {
     titlePage: "Trang chủ",
+    productsFeatured: newProducts
   });
 }
