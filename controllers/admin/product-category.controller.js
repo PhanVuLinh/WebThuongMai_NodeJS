b8caba6aed59f49ecfc9f4faefebfa4d7c2993d3
PhanVuLinh <linhphanzzz04@gmail.com>
2026-02-22 13:32:57 +0700

    Fix phân quyền bên backend

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -39,6 +39,12 @@ module.exports.create = async (req, res) => {
 
 ///[POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
+  // const permissions = res.locals.role.permissions;
+  // if(permissions.include("products-category_create")){
+  //   console.log("co quyen");
+  // } else {
+  //   return;
+  // }
   if (req.body.position == "") {
     const count = await ProductCategory.countDocuments();
     req.body.position = count + 1;
