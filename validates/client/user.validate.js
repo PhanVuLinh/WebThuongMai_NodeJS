55396c34c720717552f05abd8a3a6d1be95ffb51
PhanVuLinh <linhphanzzz04@gmail.com>
2026-02-28 15:32:29 +0700

    Đăng nhập

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -18,3 +18,19 @@ module.exports.registerPost = (req, res, next) => {
   }
   next();//--> Thỏa mãn đi sang bước tiếp theo
 }
+
+
+module.exports.loginPost = (req, res, next) => {
+  if (!req.body.email) {
+    req.flash("error", `Vui lòng nhập email`);
+    res.redirect(req.get('Referer'));
+    return;
+  }
+
+  if (!req.body.password) {
+    req.flash("error", `Vui lòng nhập mật khẩu`);
+    res.redirect(req.get('Referer'));
+    return;
+  }
+  next();//--> Thỏa mãn đi sang bước tiếp theo
+}
