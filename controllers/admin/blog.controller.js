fbf67af3f640a75aa907b303a844b7b1daefa06c
PhanVuLinh <linhphanzzz04@gmail.com>
2026-03-11 16:29:55 +0700

    Tính năng thêm mới bài viết và hiển thị

--- controllers/admin/blog.controller.js
+++ controllers/admin/blog.controller.js
@@ -1,7 +1,41 @@
+const systemConfig = require("../../config/system");
+const Blog = require("../../models/blog.model");
+
 ///[GET] /admin/blogs/
 module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false
+  }
 
+  const blog = await Blog.find(find);
   res.render("admin/pages/blogs/index", {
-    titlePage: "Tin tức"
+    titlePage: "Tin tức",
+    blog: blog
   });
+}
+
+///[GET] /admin/blogs/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/blogs/create", {
+    titlePage: "Tạo bài viết"
+  });
+}
+
+///[POST] /admin/blogs/createPost
+module.exports.createPost = async (req, res) => {
+  if (req.body.position == "") {
+    const countBlogs = await Blog.countDocuments();
+    req.body.position = countBlogs;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  if (req.file) {
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
+  };
+
+  const blog = new Blog(req.body);
+  await blog.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/blogs`);
 }
--- routes/admin/blog.route.js
+++ routes/admin/blog.route.js
@@ -1,12 +1,20 @@
 const express = require('express');
-// const multer = require('multer');
+const multer = require('multer');
 const router = express.Router();
-// const storageMulter = require("../../helpers/storageMulter");
+const storageMulter = require("../../helpers/storageMulter");
 
-// const upload = multer({ storage: storageMulter() });
+const upload = multer({ storage: storageMulter() });
 
 const controller = require("../../controllers/admin/blog.controller");
 
 router.get('/', controller.index);
+
+router.get('/create', controller.create);
+
+router.post(
+  '/create',
+  upload.single('thumbnail'),
+  controller.createPost
+);
 
 module.exports = router;
